8c7b694c826fe386c226a38f9eb97b2c4352fb8e
__Legendarytea__ <134912742+Legendarytea12@users.noreply.github.com>
2023-10-21 12:16:38 +0300

    Add files via upload

--- Chikens & Guns.py
+++ Chikens & Guns.py
@@ -11,7 +11,7 @@ pygame.init()
 
 FPS = 60
 pygame.display.set_caption("Ping-Pong")
-wind = pygame.display.set_mode((1200, 700))
+wind = pygame.display.set_mode((1200, 720))
 
 clock = pygame.time.Clock()
 
@@ -76,39 +76,35 @@ class Player(GameSprite):
             return True
         else:
             return False
-    def jump(self):
+    def start_jump(self):
         if not self.jumping and self.can_jump:
             self.jumping = True
             self.jump_count = 35
-            if self.jump_count <= 0:
-                self.jumping = False
-                if self.jump_count == 0:
-                    self.jumping = False
     def graviti(self):
         x = self.rect.x
         y = self.rect.y
-        for block in blocks:
-            if self.collide(block):
-                self.gravition = "none"
-                self.jumping = False
-                break
-            else:
-                if self.gravition == "none":
-                    self.gravition = "down"
+        
         # print(self.gravity)
-        print(self.jumping)
         if not self.jumping:
-            if self.gravition == "down":
-                self.rect.y += self.speed
-        if self.jumping == True:
-            if self.gravition == "up":
-                self.rect.y -= self.speed
+            self.rect.y += self.speed
 
-        for block in blocks:
-            if self.collide(block):
-                self.rect.x = x
-                self.rect.y = y
-                return
+            for block in blocks:
+                if self.collide(block):
+                    self.rect.x = x
+                    self.rect.y = y
+                    self.can_jump = True
+                    return
+            self.can_jump = False
+        else:
+            self.rect.y -= self.speed
+            self.jump_count -= 1
+            if self.jump_count <= 0:
+                self.jumping = False
+            for block in blocks:
+                if self.collide(block):
+                    self.rect.x = x
+                    self.rect.y = y
+                    return
             
 
 
@@ -123,6 +119,7 @@ class Enemy(GameSprite):
         self.go_left = go_left
         image = pygame.transform.scale(image, (w, h))
         self.image = image
+        enemyies.append(self)
     def move(self):
         if self.go_right == True:
             self.rect.x -= self.speed
@@ -130,11 +127,13 @@ class Enemy(GameSprite):
             self.rect.x += self.speed
 
 
+level = 1
+
+
 player_img = pygame.image.load("player_l1.png")
 
 player = Player(10, 50, 40, 40, 3, pygame.K_a , pygame.K_d, False, "right", [player_img,pygame.transform.flip(player_img, True, False)])
 enemy1_img = pygame.image.load("enemy_img.png")
-enemy1 = Enemy(450, 200, 40, 40, enemy1_img, 5 , False, True)
 
 gold = pygame.image.load("chest.png")
 kmn = pygame.image.load("дёрн.png")
@@ -142,6 +141,7 @@ invisible_block = pygame.image.load("invis_img.png")
 blocks_gold = []
 blocks = []
 blocks_inviss = []
+enemyies = []
 block_size_y = 30
 block_size_x = 30
 
@@ -149,23 +149,42 @@ x, y = 0, 0
 
 
 
-
-
-for bl in map1:
-    for l in bl:
-        if l == "2":
-            block_gold = GameSprite(x ,y , block_size_x, block_size_y, gold)
-            blocks.append(block_gold)
-        if l == "1":
-            block = GameSprite(x ,y , block_size_x, block_size_y, kmn)
-            blocks.append(block)
-        if l == "3":
-            block_invis = GameSprite(x ,y , block_size_x, block_size_y, invisible_block)
-            blocks_inviss.append(block_invis)
-        x += block_size_x
-    y += block_size_y
-    x = 0
-
+if level == 1:
+    for bl in map1:
+        for l in bl:
+            if l == "2":
+                block_gold = GameSprite(x ,y , block_size_x, block_size_y, gold)
+                blocks.append(block_gold)
+            elif l == "1":
+                block = GameSprite(x ,y , block_size_x, block_size_y, kmn)
+                blocks.append(block)
+            elif l == "3":
+                block_invis = GameSprite(x ,y , block_size_x, block_size_y, invisible_block)
+                blocks_inviss.append(block_invis)
+            elif l == "4":
+                ran = random.randint(0,1)
+                enemy = Enemy(x, y, 40, 30, enemy1_img, 5 , ran, 1-ran)
+            x += block_size_x
+        y += block_size_y
+        x = 0
+if level == 2:
+    for bl in map1:
+        for l in bl:
+            if l == "2":
+                block_gold = GameSprite(x ,y , block_size_x, block_size_y, gold)
+                blocks.append(block_gold)
+            elif l == "1":
+                block = GameSprite(x ,y , block_size_x, block_size_y, kmn)
+                blocks.append(block)
+            elif l == "3":
+                block_invis = GameSprite(x ,y , block_size_x, block_size_y, invisible_block)
+                blocks_inviss.append(block_invis)
+            elif l == "4":
+                ran = random.randint(0,1)
+                enemy = Enemy(x, y, 40, 30, enemy1_img, 5 , ran, 1-ran)
+            x += block_size_x
+        y += block_size_y
+        x = 0
 game = True
 finish = True
 
@@ -189,18 +208,8 @@ while game:
 
 
     wind.blit(bk, (0,0))
-    if pygame.sprite.spritecollide(enemy1, blocks_inviss, False):
-        if enemy1.go_right == True:
-            enemy1.go_left = True
-            enemy1.go_right = False
-        else:
-            enemy1.go_left = False
-            enemy1.go_right = True
-    if pygame.sprite.spritecollide(player, blocks_gold, False):
-        finish = True
-    #if pygame.sprite.spritecollide(enemy1, player, False):
-        #player.rect.y = 900
-        #finish = True
+    if player.collide(blocks_gold):
+        print("hello")
     if player.player_anim == "right":
         player.image = player.images[0]
     if player.player_anim == "left":
@@ -211,7 +220,7 @@ while game:
                 player.gravition = "none"
             else:
                 player.gravition = "down"
-    if player.rect.y >= 800:
+    if player.rect.y >= 900:
         music_over = pygame.mixer.Sound("GameOver.ogg")
         music_over.set_volume(0.5)
         music_over.play()
@@ -220,14 +229,30 @@ while game:
         game_over = font.render('Game Over', True, (255,255,255))
         wind.blit(game_over, (600, 0))
     if not finish:
-        enemy1.move()
-        player.move()
+        for enemy in enemyies:
+            if pygame.sprite.spritecollide(enemy, blocks_inviss, False):
+                if enemy.go_right == True:
+                    enemy.go_left = True
+                    enemy.go_right = False
+                else:
+                    enemy.go_left = False
+                    enemy.go_right = True
+    
+            if player.collide(enemy):
+                player.rect.y = 900
+                finish = True
+            enemy.move()
+            enemy.draw()
+        player.move() 
         player.graviti()
 
-    enemy1.draw()
+
+
     player.draw()
     block.draw()
-    if player.rect.y <= 700:
+
+
+    if player.rect.y <= 720:
         if finish == True:
             game_paused = font.render('Game Paused', True, (255,255,255))
             wind.blit(game_paused, (600, 0))
@@ -237,23 +262,22 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
         if event.type == pygame.KEYDOWN:
             if finish == True:
                 if event.key == pygame.K_RETURN:
                     finish = False
                     player.jumping = False
-                    enemy1.go_left = True
-                    enemy1.go_right = False
+                    enemy.go_left = True
+                    enemy.go_right = False
                     player.rect.y = 50
                     player.rect.x = 10
-                    enemy1.rect.y = 200
-                    enemy1.rect.x = 450
-            if event.key == pygame.K_SPACE:
-                if not player.jumping:
-                    player.jumping = True
-                    player.gravition = "up"
+                    enemy.rect.y = 200
+                    enemy.rect.x = 450
+            else:
+                if event.key == pygame.K_SPACE:
+                    if not player.jumping and player.can_jump:
+                        player.start_jump()
+
 
     
 
